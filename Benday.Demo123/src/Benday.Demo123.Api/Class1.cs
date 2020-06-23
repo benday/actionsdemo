de8bf41d248849d11d772be6e29c23fa8b71d596
Benjamin Day <benday@benday.com>
2020-06-23 10:49:50 -0400

    starting basic demo app

---
+++ Benday.Demo123/src/Benday.Demo123.Api/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Benday.Demo123.Api
+{
+    public class Class1
+    {
+    }
+}
---
+++ Benday.Demo123/src/Benday.Demo123.WebUi/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Benday.Demo123.WebUi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
